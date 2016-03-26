3b9c1de46107b61ef4aa9539c9bdc6ccd60bcf98
Artem Ankudovich <artemankudovich@gmail.com>
2016-03-26 12:25:42 +0000

    added a created order and cancelled order emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,14 @@ class UserMailer < ActionMailer::Base
 		 mail(:to => "#{user.name} <#{user.email}>", :subject => "Welcome To WareHouse shop")
 
 	end
+	def created_order_email(user)
+		@user=user
+		 mail(:to => "#{user.name} <#{user.email}>", :subject => "Created new order")
+
+	end
+	def cancelled_order_email(user)
+		@user=user
+		 mail(:to => "#{user.name} <#{user.email}>", :subject => "Cancelled order")
+
+	end
 end
