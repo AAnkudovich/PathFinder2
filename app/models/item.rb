611bfa1851c5560cea5c4e486398c9ab3f4cf54e
Artem Ankudovich <artemankudovich@gmail.com>
2016-04-08 17:23:59 +0100

    search now works searching for item names or partial matches

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,9 @@ class Item < ActiveRecord::Base
 	has_many :lineitems, dependent: :destroy
 	belongs_to :location
 	has_one :box
+
+	def self.search(search)
+	  where("title LIKE ?", "%#{search}%") 
+	  
+	end
 end
