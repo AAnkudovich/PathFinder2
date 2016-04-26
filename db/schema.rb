cec7ef72c4d2e10621d1e35ab6ab817048c54a9c
Artem Ankudovich <artemankudovich@gmail.com>
2016-04-26 09:41:11 +0100

    added indication of outstanding notifications

--- db/schema.rb
+++ db/schema.rb
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 20160425170946) do
     t.integer  "toID",        limit: 4
     t.text     "message",     limit: 65535
     t.integer  "regardingID", limit: 4
-    t.boolean  "seen"
+    t.boolean  "seen",                      default: false
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
   end
