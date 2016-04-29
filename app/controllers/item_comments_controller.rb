d02d1a01bf534a5b7afa480dc8c5486fcf78cc82
Artem Ankudovich <artemankudovich@gmail.com>
2016-04-29 07:17:10 +0100

    removed edit and destroy buttons just in case

--- app/controllers/item_comments_controller.rb
+++ app/controllers/item_comments_controller.rb
@@ -32,7 +32,7 @@ class ItemCommentsController < ApplicationController
         @item = Item.find(@item_comment.itemID);
         @message= "Item was reviewed"
         @notification = createAdminNotification(@item.id, @message)
-        format.html { redirect_to @item_comment, notice: 'Item comment was successfully created.' }
+        format.html { redirect_to :back, notice: 'Item comment was successfully created.' }
         format.json { render :show, status: :created, location: @item_comment }
       else
         format.html { render :new }
